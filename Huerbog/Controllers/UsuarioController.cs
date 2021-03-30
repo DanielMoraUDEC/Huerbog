fded81395587ce50928c59f7b18c38664e0e8e89
DANIEL JOSE MORA <djosemora@ucundinamarca.edu.co>
2021-03-30 09:59:58 -0500

    Nuevo controlador

---
+++ Huerbog/Controllers/UsuarioController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+
+namespace Huerbog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        
+        [HttpGet]
+        public IActionResult get()
+        {
+            
+            using (Models.HUERBOGContext u = new Models.HUERBOGContext())
+            {
+                /*  var lst = (from d in u.Usuario
+                             select d).ToList();
+                      )
+                  return Ok(lst);*/
+
+                return Ok(u.Usuarios.ToList());
+            }
+
+
+            
+        }
+        
+    }
+}
