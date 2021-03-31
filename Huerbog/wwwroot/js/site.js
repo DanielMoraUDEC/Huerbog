c5582fc24e22d64acfb801978d61041a3695c359
DANIEL JOSE MORA <djosemora@ucundinamarca.edu.co>
2021-03-31 17:14:08 -0500

    Pagina html

--- Huerbog/wwwroot/js/site.js
+++ Huerbog/wwwroot/js/site.js
@@ -2,3 +2,21 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+var url = "https://localhost:44325/api/Usuario";
+
+get();
+
+function get() {
+    fetch(url).then(function (response) {
+        return response.json();
+    }).then(function (data) {
+        document.getElementById("divlista").innerHTML = "";
+        for (i = 0; i < data.length; i++) {
+            let divElement = document.createElement("div");
+            divElement.innerHTML = data[i].nombre;
+            document.getElementById("divlista").appendChild(divElement);
+        }
+
+    })
+}
