dc622d2d8f8d801f9503971eeff26820d404359e
Daniel-code666 <dmartinezcifuentes180@gmail.com>
2021-04-02 19:21:06 -0500

    Unión API + MVC, el request POST se hace a través de la vista post.

---
+++ Huerbog/Controllers/MVC Controller/UsuariosControllerMVC.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text.Json;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace Huerbog.Controllers
+{
+    public class UsuariosControllerMVC : Controller
+    {
+        [HttpGet]
+        public ActionResult get()
+        {
+            //IEnumerable<Models.Usuario> u = null;
+
+            Models.Usuario u = null;
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:44325/api/Usuarios");
+
+                var responseTask = client.GetAsync("get");
+
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+
+                if(result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<Models.Usuario>();
+                    
+                    readTask.Wait();
+
+                    u = readTask.Result;
+                }
+                else //web api sent error response 
+                {
+                    //log response status here..
+
+                    //u = (Models.Usuario)Enumerable.Empty<Models.Usuario>();
+
+                    ModelState.AddModelError(string.Empty, "Server error. Please contact administrator.");
+                }
+            }
+            return View(u);
+        }
+
+        [HttpGet]
+        public IActionResult post()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult post(Models.Request.UserHuertaModel model)
+        {
+            HttpClient hc = new HttpClient();
+            hc.BaseAddress = new Uri("https://localhost:44325/api/Usuarios");
+
+            var insertrec = hc.PostAsJsonAsync<Models.Request.UserHuertaModel>("Usuarios", model);
+
+            insertrec.Wait();
+
+            ViewBag.message = "Usuario registrado correctamente";
+
+
+            ModelState.Clear();
+
+            return View();
+
+        }
+    }
+}
--- Huerbog/Controllers/UsuarioController.cs
+++ Huerbog/Controllers/UsuarioController.cs
@@ -1,12 +1,27 @@
 ﻿using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
 using System.Threading.Tasks;
 
-
+/*NO USAR, NO LO BORRO POR SI ALGO, PARA LA API USAR EL QUE ESTÁ EN LA CARPETA API CONTROLLER*/
+/*NO USAR, NO LO BORRO POR SI ALGO, PARA LA API USAR EL QUE ESTÁ EN LA CARPETA API CONTROLLER*/
+/*NO USAR, NO LO BORRO POR SI ALGO, PARA LA API USAR EL QUE ESTÁ EN LA CARPETA API CONTROLLER*/
+/*NO USAR, NO LO BORRO POR SI ALGO, PARA LA API USAR EL QUE ESTÁ EN LA CARPETA API CONTROLLER*/
+/*NO USAR, NO LO BORRO POR SI ALGO, PARA LA API USAR EL QUE ESTÁ EN LA CARPETA API CONTROLLER*/
+/*NO USAR, NO LO BORRO POR SI ALGO, PARA LA API USAR EL QUE ESTÁ EN LA CARPETA API CONTROLLER*/
+/*NO USAR, NO LO BORRO POR SI ALGO, PARA LA API USAR EL QUE ESTÁ EN LA CARPETA API CONTROLLER*/
+/*NO USAR, NO LO BORRO POR SI ALGO, PARA LA API USAR EL QUE ESTÁ EN LA CARPETA API CONTROLLER*/
+/*NO USAR, NO LO BORRO POR SI ALGO, PARA LA API USAR EL QUE ESTÁ EN LA CARPETA API CONTROLLER*/
+/*NO USAR, NO LO BORRO POR SI ALGO, PARA LA API USAR EL QUE ESTÁ EN LA CARPETA API CONTROLLER*/
+/*NO USAR, NO LO BORRO POR SI ALGO, PARA LA API USAR EL QUE ESTÁ EN LA CARPETA API CONTROLLER*/
+/*NO USAR, NO LO BORRO POR SI ALGO, PARA LA API USAR EL QUE ESTÁ EN LA CARPETA API CONTROLLER*/
 
 namespace Huerbog.Controllers
 {
@@ -20,36 +35,64 @@ namespace Huerbog.Controllers
        [HttpGet]
         public ActionResult get()
         {
-            using (Models.HUERBOGContext u = new Models.HUERBOGContext())
+            using (Models.HUERBOGContext db = new Models.HUERBOGContext())
             {
-                return Ok(u.Usuarios.ToList());
+                var userHuertaList = db.Usuarios.FromSqlRaw("Exec UserAndHuertaSelect");
+
+                return Ok(db.Usuarios.FromSqlRaw("Exec UserAndHuertaSelect").ToList());
             }
         }
-        
+
         [HttpPost]
-        public ActionResult post([FromBody] Models.Usuario model)
+        public ActionResult post([FromBody] Models.Request.UserHuertaModel model)
         {
            /* var existsMail = check_email(model.Correo);
 
-            var existsTelNumber = check_tel_number(model.Telefono); 
+            var existsTelNumber = check_tel_number(model.Telefono);
+
+            //var existsUbicacion = check_ubicacionHuerta(model.UbicacionHuerta);
 
             if (existsMail || existsTelNumber)
             {
-                ModelState.AddModelError("existNumberOrMail", "El correo o el número de tel ya existe");
+                ModelState.AddModelError("existNumberOrMail", "El correo, el número de tel o la ubicación de la huerta ya existe");
 
-                return Ok("El correo o el número de tel ya existe");
+                return Ok("El correo, el número de tel o la ubicación de la huerta ya existe");
             }
            */
             using (Models.HUERBOGContext u = new Models.HUERBOGContext())
+
+            using (Models.HUERBOGContext db = new Models.HUERBOGContext())
             {
                 Models.Usuario oUsuar = new Models.Usuario();
+
+                Models.TablaHuertum tHuerta = new Models.TablaHuertum();
 
                 oUsuar.Nombre = model.Nombre;
                 oUsuar.Correo = model.Correo;
                 oUsuar.Apellido = model.Apellido;
                 oUsuar.Contraseña = model.Contraseña;
-                u.Usuarios.Add(oUsuar);
-                u.SaveChanges();
+                oUsuar.Red = model.Red;
+                oUsuar.Telefono = model.Telefono;
+                tHuerta.UbicacionHuerta = model.UbicacionHuerta;
+                tHuerta.DescHuerta = model.DescHuerta;
+                tHuerta.AreaCultivo = model.AreaCultivo;
+
+                var uNombre = new SqlParameter("@nombre", oUsuar.Nombre);
+                var uApellido = new SqlParameter("@apellido", oUsuar.Apellido);
+                var uCorreo = new SqlParameter("@correo", oUsuar.Correo);
+                var uContraseña = new SqlParameter("@contraseña", oUsuar.Contraseña);
+                var uRed = new SqlParameter("@red", oUsuar.Red);
+                var uTelefono = new SqlParameter("@telefono", oUsuar.Telefono);
+                var hUbicacionHuerta = new SqlParameter("@ubicacionHuerta", tHuerta.UbicacionHuerta);
+                var hDescHuerta = new SqlParameter("@descHuerta", tHuerta.DescHuerta);
+                var hAreaCultivo = new SqlParameter("@areaCultivo", tHuerta.AreaCultivo);
+
+                var idUserReg = db.Usuarios.FromSqlRaw("Exec UserAndHuertaInsert @nombre, @apellido, @correo, @contraseña, @red, @telefono," +
+                    "@ubicacionHuerta, @descHuerta, @areaCultivo",
+                    new[] { uNombre, uApellido, uCorreo, uContraseña, uRed, uTelefono, hUbicacionHuerta, hDescHuerta, hAreaCultivo });
+
+                db.SaveChanges();
+
             }
             return Ok();
         }
@@ -129,6 +172,16 @@ namespace Huerbog.Controllers
             using (Models.HUERBOGContext db = new Models.HUERBOGContext())
             {
                 var check = db.Usuarios.Where(x => x.Telefono == telefono).FirstOrDefault();
+
+                return check != null;
+            }
+        }
+
+        public bool check_ubicacionHuerta(string ubicacion)
+        {
+            using (Models.HUERBOGContext db = new Models.HUERBOGContext())
+            {
+                var check = db.TablaHuerta.Where(x => x.UbicacionHuerta == ubicacion).FirstOrDefault();
 
                 return check != null;
             }
