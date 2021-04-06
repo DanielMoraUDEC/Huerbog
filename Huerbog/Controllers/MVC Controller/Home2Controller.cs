13cb32f860a7b9f5a7b9eff4c41655074979a406
DANIEL JOSE MORA <djosemora@ucundinamarca.edu.co>
2021-04-05 19:17:11 -0500

    IndexRegistrado

---
+++ Huerbog/Controllers/MVC Controller/Home2Controller.cs
@@ -0,0 +1,38 @@
+﻿using Huerbog.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Huerbog.Controllers.MVC_Controller
+{
+    public class Home2Controller : Controller
+    {
+
+        private readonly ILogger<Home2Controller> _logger;
+
+        public Home2Controller(ILogger<Home2Controller> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult IndexRegistrado()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
