9bd84ab15fe38cca5121f752f2cab3e1f597dd44
Daniel-code666 <dmartinezcifuentes180@gmail.com>
2021-04-27 15:14:27 -0500

    Clases necesarias

---
+++ Huerbog/Models/UserList/UserForoModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Huerbog.Models.UserList
+{
+    public class UserForoModel
+    {
+        public Usuario user { get; set; }
+        public TablaHuertum userHuerta { get; set; }
+        public IList<Foro> userForo { get; set; }
+
+    }
+}
