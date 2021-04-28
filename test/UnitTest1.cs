a0adf03230ca0536abdf0512b24c2ce833dcdaa0
JIMMY LEONARDO RUEDA GUEVARA <jleonardorueda@ucundinamarca.edu.co>
2021-04-27 21:44:24 -0500

    Conexion base de datos Azure

--- test/UnitTest1.cs
+++ test/UnitTest1.cs
@@ -1,13 +1,30 @@
+using Huerbog.Controllers.API_Controller;
+using Huerbog.Models;
+using Huerbog.Models.ForoList;
+using Huerbog.Models.Request;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace test
 {
     [TestClass]
     public class UnitTest1
     {
+        
         [TestMethod]
-        public void TestMethod1()
+        
+        public void getUser()
         {
+
+            Mock < IActionResult > Forolist = new Mock<IActionResult>();
+            Mock <IList> IList = new Mock <IList>();
+              
+            
 
         }
     }
