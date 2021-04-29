098bbc55a8512e21d02af476d46567f6ef1f3c86
brayan <bcardenas656@hotmail.com>
2021-04-28 19:52:33 -0500

    metodo pruebas unitarias incompleto

--- test/UnitTest1.cs
+++ test/UnitTest1.cs
@@ -9,7 +9,7 @@ namespace test
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1PurbametodoDelete()//este metodo realiza un test a eliminar post, asegurarse de enviar el id correcto
+        public void TestMethod1PurbaMetodoDelete()//este metodo realiza un test a eliminar post, asegurarse de enviar el id correcto
         {
             //arrange/ preparar el ambiente de nuestra prueba
             AdminController test1 = new AdminController();
@@ -19,6 +19,19 @@ namespace test
 
             //asert/ validacion de nuestro metodo
             Assert.IsTrue(resultado.IsCompletedSuccessfully);
+        }
+
+        [TestMethod]
+        public void testDeleteUser()
+        {
+            //arrange/ preparar el ambiente de nuestra prueba
+            AdminController test1 = new AdminController();
+
+            //act/ ejecucion de nuestro metodo prueba
+            var resultado = test1.deleteUser(10);//aqui se envia el id
+
+            //asert/ validacion de nuestro metodo
+            
         }
     }
 }
