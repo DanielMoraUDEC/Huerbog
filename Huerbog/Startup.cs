4b940fb68e389c92eea36a2e439255553af137d5
Daniel-code666 <dmartinezcifuentes180@gmail.com>
2021-05-02 22:50:49 -0500

    Eliminación código no usable en Startup

--- Huerbog/Startup.cs
+++ Huerbog/Startup.cs
@@ -33,8 +33,6 @@ namespace Huerbog
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            //var key = Encoding.ASCII.GetBytes(Configuration.GetValue<string>("SecretKey"));
-
             services.AddControllersWithViews();
             services.AddCors(options =>
             {
@@ -46,25 +44,6 @@ namespace Huerbog
             });
 
             services.AddDistributedMemoryCache();
-
-            //services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer();
-
-            /*services.AddAuthentication(x =>
-            {
-                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-            }).AddJwtBearer(x =>
-            {
-                x.RequireHttpsMetadata = false;
-                x.SaveToken = true;
-                x.TokenValidationParameters = new TokenValidationParameters
-                {
-                    ValidateIssuerSigningKey = true,
-                    IssuerSigningKey = new SymmetricSecurityKey(key),
-                    ValidateIssuer = false,
-                    ValidateAudience = false
-                };
-            });*/
 
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
                 .AddJwtBearer(options =>
