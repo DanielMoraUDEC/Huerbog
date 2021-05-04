d5968c6bab2fbac854c97f380b3ef983988b47cc
Daniel-code666 <dmartinezcifuentes180@gmail.com>
2021-05-04 17:14:57 -0500

    Modelo de reacciones (en progreso)

---
+++ Huerbog/Models/Reacciones/UserReaccionesModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Huerbog.Models.Reacciones
+{
+    public class UserReaccionesModel
+    {
+        public int idForo { get; set; }
+
+        public string idUser { get; set; }
+    }
+}
